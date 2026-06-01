da7c288dbc710a6ded716a5d9a667964f65123d3
Radar-Tech211 <radartech211@gmail.com>
2026-06-01 23:34:20 +0000

    adiciona vercel analytics

--- radar-site/app/layout.tsx
+++ radar-site/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import "./globals.css";
 
@@ -62,7 +63,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+     <body>{children}<Analytics /></body>
     </html>
   );
 }
